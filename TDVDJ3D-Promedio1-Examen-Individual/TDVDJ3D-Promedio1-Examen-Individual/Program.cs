b73bb509caceb4a3f69f5013b5542124db0ee459
Mangel <datcom221@gmail.com>
2026-04-30 09:40:42 -0500

    Create Project and .gitignore

---
+++ TDVDJ3D-Promedio1-Examen-Individual/TDVDJ3D-Promedio1-Examen-Individual/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDVDJ3D_Promedio1_Examen_Individual
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
